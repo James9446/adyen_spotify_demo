e7a140a1144711d8be8278de7cca2f54c5c58435
James9446 <jameshreynolds@gmail.com>
2024-10-05 01:24:58 -0700

    Updated payment completed messaging.

--- public/checkout.js
+++ public/checkout.js
@@ -34,11 +34,11 @@ async function initializeDropin() {
                 {quantity: 1, amountIncludingTax: 10000 , description: "Premium Membership"},
             ]
         }
-        // Init Sessions
-        const checkoutSessionResponse = await callServer(`/api/sessions`, checkoutDetials);
+        // Init Session
+        const session = await callServer(`/api/sessions`, checkoutDetials);
 
-        // Create AdyenCheckout using Sessions response
-        const checkout = await createAdyenCheckout(checkoutSessionResponse);
+        // Create AdyenCheckout instance using the session returned by the server
+        const checkout = await createAdyenCheckout(session);
 
         // Create an instance of Drop-in and mount it
         checkout.create("dropin", {instantPaymentTypes: ['googlepay']}).mount(document.getElementById('dropin-container'));
@@ -48,19 +48,7 @@ async function initializeDropin() {
     }
 }
 
-async function finalizeSession() {
-    try {
-        // Create AdyenCheckout re-using existing Session
-        const checkout = await createAdyenCheckout({id: sessionId});
-
-        // Submit the extracted redirectResult - triggers the onPaymentCompleted() handler  
-        checkout.submitDetails({details: {redirectResult}});
-    } catch (error) {
-        console.error(error);
-        alert("Error occurred. Look at console for details");
-    }
-}
-
+// Create AdyenCheckout instance
 async function createAdyenCheckout(session) {
     const clientKey = await getClientKey();
     const amount = session.amount;
@@ -69,7 +57,7 @@ async function createAdyenCheckout(session) {
         locale: "en_US",
         environment: "test",  // change to live for production
         showPayButton: true,
-        session: session,
+        session,
         analytics: {
             enabled: true
         },
@@ -106,18 +94,21 @@ async function createAdyenCheckout(session) {
   return new AdyenCheckout(configuration);
 }
 
-async function callServer(url, data) {
-  const response = await fetch(url, {
-      method: "POST",
-      body: data ? JSON.stringify(data) : "",
-      headers: {
-          "Content-Type": "application/json",
-      },
-  });
+// Finalize the session
+async function finalizeSession() {
+    try {
+        // Create AdyenCheckout instance re-using existing Session
+        const checkout = await createAdyenCheckout({id: sessionId});
 
-  return await response.json();
+        // Submit the extracted redirectResult - triggers the onPaymentCompleted() handler  
+        checkout.submitDetails({details: {redirectResult}});
+    } catch (error) {
+        console.error(error);
+        alert("Error occurred. Look at console for details");
+    }
 }
 
+// Handle the payment result
 function handlePaymentResult(response, component) {
     if (response.action) {
         component.handleAction(response.action);
@@ -126,11 +117,16 @@ function handlePaymentResult(response, component) {
             case "Authorised":
                 changeCheckoutTitle("Payment Completed");
                 setTimeout(addPaymentCompleteMessage, 2000);
-                setTimeout(addReturnHomeButton, 4000);
+                setTimeout(addButton, 3000);
                 console.log("response.resultCode: ", response.resultCode);
                 break;
             case "Refused":
                 changeCheckoutTitle("Payment Refused");
+                setTimeout(() => {
+                    addPaymentCompleteMessage("We encountered a problem while processing your payment method.");
+                    addPaymentCompleteMessage("Please try again, or choose a different payment method to complete your purchase.");
+                    addButton("/checkout", "Continue");
+                }, 1500);
                 console.log("response.resultCode: ", response.resultCode);
                 break;
             case "Pending":
@@ -144,6 +140,19 @@ function handlePaymentResult(response, component) {
                 break;
         }
     }
+}
+
+// Call server
+async function callServer(url, data) {
+    const response = await fetch(url, {
+        method: "POST",
+        body: data ? JSON.stringify(data) : "",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+  
+    return await response.json();
 }
 
 // ----- Utility functions ------
@@ -163,33 +172,33 @@ function changeCheckoutTitle(newTitle) {
   }
 }
 
-function addPaymentCompleteMessage() {
+function addPaymentCompleteMessage(message="Welcome to Spotify Premium! Enjoy music like never before!") {
   const container = document.querySelector('.checkout-container');
   
-  // Add thank you message
-  const thankYouMessage = document.createElement('p');
-  thankYouMessage.textContent = "Congratulations on joining Spotify Premium!";
-  thankYouMessage.style.marginTop = '20px';
-  container.appendChild(thankYouMessage);
+  // Add payment complete message after session is complete
+  const paymentCompleteMessage = document.createElement('p');
+  paymentCompleteMessage.textContent = message;
+  paymentCompleteMessage.style.marginTop = '20px';
+  container.appendChild(paymentCompleteMessage);
 }
 
-function addReturnHomeButton() {
+function addButton(href="/", buttonText="Explore Your Benefits") {
   const container = document.querySelector('.checkout-container');
   
   // Add button to navigate back to homepage
-  const homeButton = document.createElement('button');
-  homeButton.textContent = "Learn About New Features";
-  homeButton.style.marginTop = '20px';
-  homeButton.style.padding = '10px 20px';
-  homeButton.style.backgroundColor = '#1DB954';
-  homeButton.style.color = 'white';
-  homeButton.style.border = 'none';
-  homeButton.style.borderRadius = '20px';
-  homeButton.style.cursor = 'pointer';
+  const button = document.createElement('button');
+  button.textContent = buttonText;
+  button.style.marginTop = '20px';
+  button.style.padding = '10px 20px';
+  button.style.backgroundColor = '#1DB954';
+  button.style.color = 'white';
+  button.style.border = 'none';
+  button.style.borderRadius = '20px';
+  button.style.cursor = 'pointer';
   
-  homeButton.addEventListener('click', () => {
-      window.location.href = '/'; // Adjust this if your homepage URL is different
+  button.addEventListener('click', () => {
+      window.location.href = href; // Adjust this if your homepage URL is different
   });
   
-  container.appendChild(homeButton);
+  container.appendChild(button);
 }
